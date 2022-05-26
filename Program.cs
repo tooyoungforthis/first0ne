03066ea3fa0d3389121b4cd117f4433648c13b7b
tooyoungforthis <105984023+tooyoungforthis@users.noreply.github.com>
2022-05-26 21:54:12 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -13,9 +13,37 @@ namespace unban
     {
         static void PrintCommands()
         {
+            Console.WriteLine();
             Console.WriteLine("press 1 to create new entry" + "\n" + "press 2 to edit the entry" + "\n" + "press 3 to delete the entry");
             Console.WriteLine("press 4 to view created entry" + "\n" + "press 5 to view all summary entries");
         }
+
+
+        public static bool CheckPhoneNumber(string number)
+        {
+            if (number.Length == 0) return false;
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                if (Char.IsDigit(number[i])) continue;
+                else return false;
+            }
+            return true;
+        }
+
+        public static bool IsNeccesaryValue(string value)
+        {
+            if (value.Length == 0)
+            {
+                Console.WriteLine("Required to FILL");
+                return false;
+            }
+            return true;
+        }
+
+
+
+
         public static void Main(string[] args)
         {
             PrintCommands();
@@ -34,40 +62,66 @@ namespace unban
                     }
                     else if (pressedButton == 2)
                     {
-                        if (PhoneBook.phonebook.Count == 0) Console.WriteLine("need to create new entry");
+
+                        if (PhoneBook.phonebook.Count == 0) Console.WriteLine("NEED TO CREATE NEW ENTRY");
                         else
                         {
-                            Console.Write($"Enter the number of the editing entry, from 0 to {PhoneBook.phonebook.Count - 1}: ");
-                            int entryNumber = int.Parse(Console.ReadLine());
-                            if (entryNumber <= PhoneBook.phonebook.Count-1) phonebook.EditingTheEntry(entryNumber);
-                            else Console.WriteLine($"Last entry number is {PhoneBook.phonebook.Count - 1}");
+                            while (true)
+                            {
+                                Console.Write($"Enter the number of the editing entry, from 1 to {PhoneBook.phonebook.Count}: ");
+                                int entryNumber;
+                                bool result = int.TryParse(Console.ReadLine(), out entryNumber);
+                                if (entryNumber <= PhoneBook.phonebook.Count && entryNumber >= 1 && result)
+                                {
+                                    phonebook.EditingTheEntry(entryNumber);
+                                    break;
+                                }
+                                else Console.WriteLine($"LAST entry NUMBER is {PhoneBook.phonebook.Count}, try again");
+                            }
+
                         }
                     }
                     else if (pressedButton == 3)
                     {
-                        if (PhoneBook.phonebook.Count == 0) Console.WriteLine("need to create new entry");
+                        if (PhoneBook.phonebook.Count == 0) Console.WriteLine("NEED TO CREATE NEW ENTRY");
                         else
                         {
-                            Console.Write($"Enter the number of the deleting entry, from 0 to {PhoneBook.phonebook.Count - 1}: ");
-                            int entryNumber = int.Parse(Console.ReadLine());
-                            if (entryNumber <= PhoneBook.phonebook.Count-1) phonebook.DeletingTheEntry(entryNumber);
-                            else Console.WriteLine($"Last entry number is {PhoneBook.phonebook.Count - 1}");
+                            while (true)
+                            {
+                                Console.Write($"Enter the number of the deleting entry, from 1 to {PhoneBook.phonebook.Count}: ");
+                                int entryNumber;
+                                bool result = int.TryParse(Console.ReadLine(), out entryNumber);
+                                if (entryNumber <= PhoneBook.phonebook.Count && entryNumber >= 1 && result)
+                                {
+                                    phonebook.DeletingTheEntry(entryNumber);
+                                    break;
+                                }
+                                else Console.WriteLine($"LAST entry NUMBER is {PhoneBook.phonebook.Count},try again");
+                            }
                         }
                     }
                     else if (pressedButton == 4)
                     {
-                        if (PhoneBook.phonebook.Count == 0) Console.WriteLine("need to create new entry");
+                        if (PhoneBook.phonebook.Count == 0) Console.WriteLine("NEED TO CREATE NEW ENTRY");
                         else
                         {
-                            Console.Write($"Enter the number of the viewing entry, from 0 to {PhoneBook.phonebook.Count - 1}: ");
-                            int entryNumber = int.Parse(Console.ReadLine());
-                            if (entryNumber <= PhoneBook.phonebook.Count-1) phonebook.ViewingCreatedEntry(entryNumber);
-                            else Console.WriteLine($"Last entry number is {PhoneBook.phonebook.Count - 1}");
+                            while (true)
+                            {
+                                Console.Write($"Enter the number of the viewing entry, from 1 to {PhoneBook.phonebook.Count}: ");
+                                int entryNumber;
+                                bool result = int.TryParse(Console.ReadLine(), out entryNumber);
+                                if (entryNumber <= PhoneBook.phonebook.Count && entryNumber >= 1 && result)
+                                {
+                                    phonebook.ViewingCreatedEntry(entryNumber);
+                                    break;
+                                }
+                                else Console.WriteLine($"LAST entry NUMBER is {PhoneBook.phonebook.Count}, try again");
+                            }
                         }
                     }
                     else
                     {
-                        if (PhoneBook.phonebook.Count == 0) Console.WriteLine("need to create new entry");
+                        if (PhoneBook.phonebook.Count == 0) Console.WriteLine("NEED TO CREATE NEW ENTRY");
                         else phonebook.ViewingAllSummaryEntries();
                     }
                     PrintCommands();
@@ -89,11 +143,23 @@ namespace unban
         public static List<Person> phonebook = new List<Person>();
 
         public List<Person> CreationOfNewEntry()
-        {
-            Console.WriteLine("Enter surname");
-            string surname = Console.ReadLine();
-            Console.WriteLine("Enter name");
-            string name = Console.ReadLine();
+        {   
+            string surname;
+            while (true)
+            {
+                Console.WriteLine("Enter surname");
+                surname = Console.ReadLine();
+                if (Program.IsNeccesaryValue(surname)) break;
+            }
+
+            string name;
+            while (true)
+            {
+                Console.WriteLine("Enter name");
+                name = Console.ReadLine();
+                if (Program.IsNeccesaryValue(name)) break;
+            }
+
             Console.WriteLine("Enter patronymic");
             string patronymic = Console.ReadLine();
 
@@ -102,15 +168,21 @@ namespace unban
             {
                 Console.WriteLine("Enter phoneNumber, only digits allowed");
                 phonenumber = Console.ReadLine();
-                if (int.TryParse(phonenumber, out int number))
+                if (Program.CheckPhoneNumber(phonenumber))
                 {
                     break;
                 }
                 Console.WriteLine("Invalid input");
             }
 
-            Console.WriteLine("Enter country");
-            string country = Console.ReadLine();
+            string country;
+            while (true)
+            {
+                Console.WriteLine("Enter country");
+                country = Console.ReadLine();
+                if (Program.IsNeccesaryValue(country)) break;
+            }
+
             Console.WriteLine("Enter dateofbirth, FORMAT: day.month.year, EXAMPLE: 01.01.1970");
             string dateofbirth = Console.ReadLine();
             Console.WriteLine("Enter organization");
@@ -128,43 +200,88 @@ namespace unban
 
         public List<Person> EditingTheEntry(int entrynumber)
         {
-            //List<Tuple<string, string>> changes = new List<Tuple<string, string>>();
             Console.Write("1 - surname, 2 - name, 3 - patronymic, 4 - phonenumber, 5 - country, 6 - dateofbirth, ");
             Console.WriteLine("7 - organization, 8 - position, 9 - otherinformation");
+            Console.WriteLine("new value = stop to STOP changes");
             Console.WriteLine("Enter the number of parameter you want to edit and its new value: ");
             while (true)
             {
                 Console.Write("Enter the number of parameter: ");
                 int parameter;
                 int.TryParse(Console.ReadLine(), out parameter);
+                if (parameter > 9 || parameter < 1)
+                {
+                    Console.WriteLine("Invalid number of parameter, try again");
+                    continue;
+                }
                 Console.Write("Enter new value: ");
                 string newValue = Console.ReadLine();
+
+                if (newValue == "stop" || parameter == 0) break;
+
 
                 if (parameter == 4)
                 {
                     while (true)
-                    {   
-                        if (int.TryParse(newValue, out int number))
+                    {
+                        if (Program.CheckPhoneNumber(newValue))
                         {
-                            phonebook[entrynumber].PhoneNumber = newValue;
+                            phonebook[entrynumber - 1].PhoneNumber = newValue;
                             break;
                         }
                         Console.WriteLine("Only digits allowed");
                         Console.Write("Enter new value: ");
                         newValue = Console.ReadLine();
                     }
-                }   
+                }
 
-                if (newValue == "stop" || parameter == 0) break;
+                if (parameter == 1)
+                {
+                    while (true)
+                    {
+                        if (Program.IsNeccesaryValue(newValue))
+                        {
+                            phonebook[entrynumber - 1].SurName = newValue;
+                            break;
+                        }
+                        Console.Write("Enter new value: ");
+                        newValue = Console.ReadLine();
+                    }
+                }
 
-                if (parameter == 1) phonebook[entrynumber].SurName = newValue;
-                else if (parameter == 2) phonebook[entrynumber].Name = newValue;
-                else if (parameter == 3) phonebook[entrynumber].Patronymic = newValue;
-                else if (parameter == 5) phonebook[entrynumber].Country = newValue;
-                else if (parameter == 6) phonebook[entrynumber].DateOfBirth = newValue;
-                else if (parameter == 7) phonebook[entrynumber].Organization = newValue;
-                else if (parameter == 8) phonebook[entrynumber].Position = newValue;
-                else phonebook[entrynumber].OtherInformation = newValue;
+                if (parameter == 2)
+                {
+                    while (true)
+                    {
+                        if (Program.IsNeccesaryValue(newValue))
+                        {
+                            phonebook[entrynumber - 1].Name = newValue;
+                            break;
+                        }
+                        Console.Write("Enter new value: ");
+                        newValue = Console.ReadLine();
+                    }
+                }
+                
+                if (parameter == 5)
+                {
+                    while (true)
+                    {
+                        if (Program.IsNeccesaryValue(newValue))
+                        {
+                            phonebook[entrynumber - 1].Country = newValue;
+                            break;
+                        }
+                        Console.Write("Enter new value: ");
+                        newValue = Console.ReadLine();
+                    }
+                }
+
+                else if (parameter == 3) phonebook[entrynumber - 1].Patronymic = newValue;
+                else if (parameter == 6) phonebook[entrynumber - 1].DateOfBirth = newValue;
+                else if (parameter == 7) phonebook[entrynumber - 1].Organization = newValue;
+                else if (parameter == 8) phonebook[entrynumber - 1].Position = newValue;
+                else phonebook[entrynumber - 1].OtherInformation = newValue;
             }
 
             return phonebook;
@@ -172,36 +289,37 @@ namespace unban
 
         public List<Person> DeletingTheEntry(int entrynumber)
         {
-            phonebook.RemoveAt(entrynumber);
+            phonebook.RemoveAt(entrynumber - 1);
             return phonebook;
         }
 
         public void ViewingCreatedEntry(int entrynumber)
         {
-            Console.WriteLine($"surname: {phonebook[entrynumber].SurName}");
-            Console.WriteLine($"name: {phonebook[entrynumber].Name}");
-            Console.WriteLine($"patronymic {phonebook[entrynumber].Patronymic}");
-            Console.WriteLine($"phonenumber {phonebook[entrynumber].PhoneNumber}");
-            Console.WriteLine($"country: {phonebook[entrynumber].Country}");
-            Console.WriteLine($"date of birth: {phonebook[entrynumber].DateOfBirth}");
-            Console.WriteLine($"organization: {phonebook[entrynumber].Organization}");
-            Console.WriteLine($"position: {phonebook[entrynumber].Position}");
-            Console.WriteLine($"other information: {phonebook[entrynumber].OtherInformation}");
+            Console.WriteLine($"surname: {phonebook[entrynumber - 1].SurName}");
+            Console.WriteLine($"name: {phonebook[entrynumber - 1].Name}");
+            Console.WriteLine($"patronymic: {phonebook[entrynumber - 1].Patronymic}");
+            Console.WriteLine($"phonenumber: {phonebook[entrynumber - 1].PhoneNumber}");
+            Console.WriteLine($"country: {phonebook[entrynumber - 1].Country}");
+            Console.WriteLine($"date of birth: {phonebook[entrynumber - 1].DateOfBirth}");
+            Console.WriteLine($"organization: {phonebook[entrynumber - 1].Organization}");
+            Console.WriteLine($"position: {phonebook[entrynumber - 1].Position}");
+            Console.WriteLine($"other information: {phonebook[entrynumber - 1].OtherInformation}");
         }
 
         public void ViewingAllSummaryEntries()
         {
+            int i = 0;
             foreach (Person person in phonebook)
             {
-                Console.Write($"surname: {person.SurName}, ");
-                Console.Write($"name: {person.Name}, ");
-                Console.WriteLine($"phonenumber: {person.PhoneNumber}");
+                ++i;
+                Console.Write(i + ".");
+                Console.Write($" surname: {person.SurName}, " + "\t");
+                Console.Write($"name: {person.Name}, " + "\t");
+                Console.WriteLine($"phonenumber: {person.PhoneNumber}" + '\t');
 
             }
 
         }
-
-
     }
 
 
